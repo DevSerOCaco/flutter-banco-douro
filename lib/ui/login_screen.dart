e5c4f37280e574a04e8581b074ad2d03af672970
caco <pauloricardossfilho@gmail.com>
2025-12-12 12:53:43 -0300

    Primeira aula

---
+++ lib/ui/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: [
+           Text('Sostema de gestao de pontos'),
+           TextFormField(),
+           TextFormField(),
+           ElevatedButton(
+            onPressed: (){},
+             child: const Text("Entrar")),
+          ],
+        ),
+        );
+  }
+}
