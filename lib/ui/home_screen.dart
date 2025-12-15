7170bed785ec0fef1f6f03e871b6d5c56f2cb6bc
caco <pauloricardossfilho@gmail.com>
2025-12-15 13:02:56 -0300

    mais trabalho

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_banco_douro/models/account.dart';
+import 'package:flutter_banco_douro/services/account_service.dart';
 import 'package:flutter_banco_douro/ui/styles/colors.dart';
 import 'package:flutter_banco_douro/ui/widgets/account_widget.dart';
 
@@ -23,15 +24,25 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: AccountWidget(
-          account: Account(
-            id: "1",
-            name: "John",
-            lastName: "Doe",
-            balance: 1000.0,
-            accountType: "Checking",
-          ),
-        ),
+        child: FutureBuilder(future: AccountService().getAll(), builder: (context, snapshot) {
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:{
+              if(snapshot.data == null || snapshot.data!.isEmpty){
+                return const Text("Nenhuma conta recebida");
+              }
+              List<Account> listAccounts = snapshot.data!;
+              return ListView.builder(itemCount: listAccounts.length, itemBuilder: (context, index) {
+                return AccountWidget(account: listAccounts[index]);
+              },);
+            }
+          }
+        },)
       ),
     );
   }
